0d68d2fd39c23c00a6d44854b00c4c6da9e6e17a
marcenavuc <mark.averchenko@gmail.com>
2023-04-25 00:45:43 +0500

    refuctored

--- src/main/java/org/example/TestCart.java
+++ src/main/java/org/example/TestCart.java
@@ -1,54 +1,39 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+
 
 public class TestCart {
+    private static final Duration implicitWait = Duration.ofSeconds(3);
+    private static final Duration pageTimeout = Duration.ofSeconds(20);
+    private static final Dimension windowSize = new Dimension(1280, 1280);
+    private static final String  baseUrl = "https://www.dns-shop.ru/";
 
-    ChromeDriver driver = null;
-    WebDriverWait wait = null;
-    String baseUrl = "https://www.dns-shop.ru/";
+    ChromeDriver driver;
 
     @BeforeEach
-    public void setUpDriver() {
-
-        //Set up driver
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        //Set implicit wait:
-        //wait for WebElement
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-
-        //wait for loading page
-        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-
-        driver.manage().window().setSize(new Dimension(1280, 1280));
-        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(implicitWait);
+        driver.manage().timeouts().pageLoadTimeout(pageTimeout);
+        driver.manage().window().setSize(windowSize);
     }
 
-    @Test
-    public void addItemToCart() {
+    private void addItem() {
         driver.get(baseUrl);
-
-        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
-
-        //Search an element
+        driver.manage().timeouts().pageLoadTimeout(pageTimeout);
         var downPricedItems = driver.findElement(By.xpath("//span[@class='catalog-menu__root-item-markdown']"));
         downPricedItems.click();
 
@@ -56,62 +41,52 @@ public class TestCart {
         var firstHref = productNames.get(0);
         firstHref.click();
 
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         var buyButton = driver.findElement(By.xpath("//button[@class='button-ui buy-btn button-ui_passive button-ui_brand']"));
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         buyButton.click();
+        driver.manage().timeouts().implicitlyWait(implicitWait);
+    }
 
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    private void openCart() {
         var cartButton = driver.findElement(By.xpath("//a[@class='buttons__link ui-link cart-link-counter']"));
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(implicitWait);
         cartButton.click();
 
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        // Refresh the page to get actual items
+        driver.manage().timeouts().implicitlyWait(implicitWait);
         driver.navigate().refresh();
-        var itemsInCart = driver.findElements(By.xpath("//div[@class='cart-items__product cart-items__product_markdown']"));
-
-        Assertions.assertEquals(1, itemsInCart.size());
     }
 
     @Test
-    public void removeItemToCart() {
-        driver.get(baseUrl);
+    public void testAddItemToCart() {
+        addItem();
+        openCart();
 
-        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        Assertions.assertEquals(
+                1,
+                driver.findElements(By.xpath(
+                        "//div[@class='cart-items__product cart-items__product_markdown']")
+                ).size());
+    }
 
-        //Search an element
-        var downPricedItems = driver.findElement(By.xpath("//span[@class='catalog-menu__root-item-markdown']"));
-        downPricedItems.click();
+    @Test
+    public void testRemoveItemFromCart() {
+        addItem();
+        openCart();
 
-        var productNames =  driver.findElements(By.xpath("//a[@class='catalog-product__name ui-link ui-link_black']"));
-        var firstHref = productNames.get(0);
-        firstHref.click();
-
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        var buyButton = driver.findElement(By.xpath("//button[@class='button-ui buy-btn button-ui_passive button-ui_brand']"));
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        buyButton.click();
-
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        var cartButton = driver.findElement(By.xpath("//a[@class='buttons__link ui-link cart-link-counter']"));
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        cartButton.click();
-
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.navigate().refresh();
-
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         var delButtons = driver.findElements(By.xpath("//button[@class='menu-control-button remove-button']"));
         for (var button: delButtons) {
             button.click();
             driver.navigate().refresh();
         }
 
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(implicitWait);
         driver.navigate().refresh();
-        var itemsInCart = driver.findElements(By.xpath("//div[@class='cart-items__product cart-items__product_markdown']"));
 
-        Assertions.assertEquals(0, itemsInCart.size());
+        Assertions.assertEquals(
+                0,
+                driver.findElements(By.xpath(
+                        "//div[@class='cart-items__product cart-items__product_markdown']"
+                )).size());
     }
 
     @AfterEach
